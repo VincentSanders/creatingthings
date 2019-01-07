ae7803537b72415b5a7e030a9523d06488ccb728
Vincent Sanders <sandersvincent@gmail.com>
2019-01-07 21:59:46 +0000

    Ma

--- Github Finder/app.js
+++ Github Finder/app.js
@@ -1,5 +1,8 @@
-// Initialize Github
+// Init Github
 const github = new Github;
+
+// Init UI
+const ui = new UI;
 
 // Search input
 const searchUser = document.getElementById('searchUser');
@@ -13,15 +16,17 @@ searchUser.addEventListener('keyup', (e) => {
         // Make http call
         github.getUser(userText)
             .then(data => {
-                if(data.profile.message === 'Not found'){
+                if(data.profile.message === 'Not Found'){
                     // Show alert
-
+                    ui.showAlert('User not found', 'alert alert-danger');
                 }else{
                     // Show profile 
-
+                    ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
                 }
             });
     }else{
         // Clear profile
+        ui.clearProfile();
     }
 });
--- Github Finder/github.js
+++ Github Finder/github.js
@@ -2,14 +2,22 @@ class Github{
     constructor(){
         this.client_id = '1fde936feb222a689c53';
         this.client_secret = '3ecbf0b7e2f0016ca103c3743b8bf07cd90132f0';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user){
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-        const profile = await profileResponse.json();
         
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        
+        const profile = await profileResponse.json();
+
+        const repos = await repoResponse.json();
+
         return{
-            profile
+            profile, 
+            repos
         }
     }
 }
